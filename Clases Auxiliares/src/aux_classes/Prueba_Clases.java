cc6fd50f447890efb41b940e10bf4f53cf37801e
Nestor Luis Tobon <nltobon@gmail.com>
2015-03-07 22:03:04 -0430

    Agregar todos los archivos

---
+++ Clases Auxiliares/src/aux_classes/Prueba_Clases.java
@@ -0,0 +1,22 @@
+package aux_classes;
+import java.util.Scanner;
+
+
+public class Prueba_Clases{
+
+	private static Scanner input = new Scanner(System.in);
+
+	public static void main (String[] args){
+
+		System.out.print("Please enter your favorite number: ");
+		int number = input.nextInt();
+		Print.cls();
+		Print.outSln(number+"");
+		
+		
+
+
+
+
+		}//main
+}//class
